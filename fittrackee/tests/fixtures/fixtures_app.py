47ff13a4617c0fae9fe5d16d2473d6b10cc79bd8
Sam <sam@samr1.net>
2021-01-30 19:37:37 +0100

    API - Actor model update + refactor

--- fittrackee/tests/fixtures/fixtures_app.py
+++ fittrackee/tests/fixtures/fixtures_app.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from typing import Generator, Optional, Union
 
 import pytest
@@ -6,6 +7,7 @@ import pytest
 from fittrackee import create_app, db
 from fittrackee.application.models import AppConfig
 from fittrackee.application.utils import update_app_config_from_database
+from fittrackee.federation.models import Domain
 
 
 def get_app_config(
@@ -14,9 +16,11 @@ def get_app_config(
     max_single_file_size: Optional[Union[int, float]] = None,
     max_zip_file_size: Optional[Union[int, float]] = None,
     max_users: Optional[int] = None,
+    with_federation: Optional[bool] = False,
 ) -> Optional[AppConfig]:
     if with_config:
         config = AppConfig()
+        config.federation_enabled = with_federation
         config.gpx_limit_import = 10 if max_workouts is None else max_workouts
         config.max_single_file_size = (
             (1 if max_single_file_size is None else max_single_file_size)
@@ -41,6 +45,8 @@ def get_app(
     max_single_file_size: Optional[Union[int, float]] = None,
     max_zip_file_size: Optional[Union[int, float]] = None,
     max_users: Optional[int] = None,
+    with_federation: Optional[bool] = False,
+    with_domain: Optional[bool] = True,
 ) -> Generator:
     app = create_app()
     with app.app_context():
@@ -52,9 +58,14 @@ def get_app(
                 max_single_file_size,
                 max_zip_file_size,
                 max_users,
+                with_federation,
             )
             if app_db_config:
                 update_app_config_from_database(app, app_db_config)
+                if with_domain:
+                    domain = Domain(name=app.config['AP_DOMAIN'])
+                    db.session.add(domain)
+                    db.session.commit()
             yield app
         except Exception as e:
             print(f'Error with app configuration: {e}')
@@ -128,13 +139,21 @@ def app_tls(monkeypatch: pytest.MonkeyPatch) -> Generator:
 
 
 @pytest.fixture
+def app_with_federation() -> Generator:
+    yield from get_app(with_config=True, with_federation=True)
+
+
+@pytest.fixture
 def app_wo_domain() -> Generator:
-    yield from get_app(with_config=True)
+    yield from get_app(
+        with_config=True, with_federation=True, with_domain=False
+    )
 
 
 @pytest.fixture()
 def app_config() -> AppConfig:
     config = AppConfig()
+    config.federation_enabled = False
     config.gpx_limit_import = 10
     config.max_single_file_size = 1048576
     config.max_zip_file_size = 10485760
@@ -142,3 +161,10 @@ def app_config() -> AppConfig:
     db.session.add(config)
     db.session.commit()
     return config
+
+
+@pytest.fixture()
+def app_version() -> str:
+    return (
+        (Path(__file__).parent.parent.parent / 'VERSION').read_text().strip()
+    )
